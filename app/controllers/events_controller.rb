dc7198c147a21870e0e802f98068563787acf6b3
Nick Schoch <nschoch@gmail.com>
2019-08-21 09:10:53 -0700

    Added ability to create events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,8 +8,21 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = current_user.events.new
   end
 
   def create
+    @event = current_user.events.new(event_params)
+    if @event.save
+      flash.now[:info] = "Event created!"
+      redirect_to current_user
+    else
+      render 'new'
+    end
   end
+
+  private
+    def event_params
+      params.require(:event).permit(:start_date, :description)
+    end
 end
