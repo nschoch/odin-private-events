7fe2775c9f938a3be8362baae53421a33597cf68
Nick Schoch <nschoch@gmail.com>
2019-08-24 09:54:32 -0700

    complete

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,8 @@ class Event < ApplicationRecord
   has_many :invitations, foreign_key: :attended_event_id
   has_many :attendees, through: :invitations
   default_scope -> { order(start_date: :asc) }
-
+  scope :upcoming, -> { where('start_date >= :today', {today: Time.now.to_date})}
+  scope :past, -> { where('start_date < :today', {today: Time.now.to_date})}
   validates :start_date, presence: true
   validates :description, presence: true
   validates :creator_id, presence: true
